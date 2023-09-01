5ed9baa51320ea9861286921df1c56fef71c97ad
atok624 <84607674+atok624@users.noreply.github.com>
2023-09-01 23:23:09 +0000

    Update project

--- js/app.js
+++ js/app.js
@@ -1,66 +1,56 @@
-(function($, document, window){
-	
-	$(document).ready(function(){
+(function ($, document, window) {
+  $(document).ready(function () {
+    // Cloning main navigation for mobile menu
+    $(".mobile-navigation").append($(".main-navigation .menu").clone());
 
-		// Cloning main navigation for mobile menu
-		$(".mobile-navigation").append($(".main-navigation .menu").clone());
+    // Mobile menu toggle
+    $(".menu-toggle").click(function () {
+      $(".mobile-navigation").slideToggle();
+    });
 
-		// Mobile menu toggle 
-		$(".menu-toggle").click(function(){
-			$(".mobile-navigation").slideToggle();
-		});
+    $(".hero").flexslider({
+      directionNav: false,
+      controlNav: true,
+    });
 
-		$(".hero").flexslider({
-			directionNav: false,
-			controlNav: true
-		});
+    var mymap = L.map("map").setView([5.633925, -0.244143], 15);
 
-		var map = $(".map");
-		var latitude = map.data("latitude");
-		var longitude = map.data("longitude");
-		if( map.length ){
-			
-			map.gmap3({
-				map:{
-					options:{
-						center: [latitude,longitude],
-						zoom: 15,
-						scrollwheel: false
-					}
-				},
-				marker:{
-					latLng: [latitude,longitude],
-					options:{
-			          icon: "images/map-pin.png"
-			        }
-				}
-			});
-			
-		}
+    // Add a tile layer (use OpenStreetMap as the base map)
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(mymap);
 
-		// var waypoint = new Waypoint({
-		  var $section =  $(".fullwidth-block");
-		  $section.each(function(){
-		  	$(this).waypoint(function(){
-		  		$("a[href=#"+this.element.id+"]" ).parent("li").toggleClass("current-menu-item").siblings().removeClass("current-menu-item");
-		  	},{
-		  		offset: 200
-		  	});
-		  });
-		  
-
-	});
-
-	$(window).scroll(function () {
-        if ($(this).scrollTop() > 1) {
-            $('.site-header').addClass("sticky");
-        } else {
-            $('.site-header').removeClass("sticky");
+    // Add a marker to the map
+    L.marker([5.633925, -0.244143])
+      .addTo(mymap)
+      .bindPopup("Hello, Welcome to TPMG!")
+      .openPopup();
+    // var waypoint = new Waypoint({
+    var $section = $(".fullwidth-block");
+    $section.each(function () {
+      $(this).waypoint(
+        function () {
+          $("a[href=#" + this.element.id + "]")
+            .parent("li")
+            .toggleClass("current-menu-item")
+            .siblings()
+            .removeClass("current-menu-item");
+        },
+        {
+          offset: 200,
         }
-    }); 
+      );
+    });
+  });
 
-	$(window).load(function(){
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+      $(".site-header").addClass("sticky");
+    } else {
+      $(".site-header").removeClass("sticky");
+    }
+  });
 
-	});
-
+  $(window).load(function () {});
 })(jQuery, document, window);
